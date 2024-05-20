bd0c773c07a49a60fb1144d83ec029ea06b8270b
Harold Jose Montes De Oca Gonzalez <harold.montesdeoca@ikeasi.com>
2024-05-20 15:34:47 -0400

    create crud models functions

--- index.php
+++ index.php
@@ -1,18 +1,6 @@
 <?php
 
+
+
 require 'vendor/autoload.php';
 
-use Api\database\ConnectDb;
-
-
-$instance = ConnectDb::getInstance();
-$conn = $instance->getConnection();
-var_dump($conn);
-
-$instance = ConnectDb::getInstance();
-$conn = $instance->getConnection();
-var_dump($conn);
-
-$instance = ConnectDb::getInstance();
-$conn = $instance->getConnection();
-var_dump($conn);
